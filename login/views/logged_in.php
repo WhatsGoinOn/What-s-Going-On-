2e410ad905cd53264c7793d4dfdcdb1466826dcf
AndrewJGifford <gifford.andrew.j@gmail.com>
2015-03-10 14:09:54 -0500

    Fixed direct linking to login views, changed to php self references

--- login/views/logged_in.php
+++ login/views/logged_in.php
@@ -1,5 +1,5 @@
 <!-- if you need user information, just put them into the $_SESSION variable and output them here -->
-Welcome, <?php echo $_SESSION['user_email']; ?>.
+Welcome, <?php echo htmlspecialchars($_SESSION['user_email']); ?>.
 
 <!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
-<a href="/WhatsGoingOn/login/loginHandler.php?logout">Logout</a>
+<a href="<?php echo $_SERVER["PHP_SELF"] . "?logout"; ?>">Logout</a>
