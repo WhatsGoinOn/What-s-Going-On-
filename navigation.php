b753ec70efdfb3193c7ae95277417263ea67051b
AndrewJGifford <gifford.andrew.j@gmail.comgit config --global user.email gifford.andrew.j@gmail.comgit config --global user.email gifford.andrew.j@gmail.com>
2015-03-26 15:44:45 -0500

    Changed link on nav bar

--- navigation.php
+++ navigation.php
@@ -2,6 +2,6 @@
     <li><a href="default.php">Home</a></li>
     <li><a href="search.php">Search Events</a></li>
     <li><a href="map.php">Map</a></li>
-    <li><a href="createEvent.php">Create Event</a></li>
+    <li><a href="createEventHandler.php">Create Event</a></li>
     <li><a href="userProfile.php">User Profile</a></li>
 </ul>
