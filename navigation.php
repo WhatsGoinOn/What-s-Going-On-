b3561d9d82dc02635780fabc1625200dde7c471a
Chris Wizner <wizner8398@fvtc.edu>
2015-03-29 17:11:41 -0500

    updated links

--- navigation.php
+++ navigation.php
@@ -1,12 +1,12 @@
 <ul>
     <li><a href="/WhatsGoingOn/default.php">Home</a></li>
-    <li><a href="/WhatsGoingOn/views/search.php">Search Events</a></li>
+    <li><a href="/WhatsGoingOn/views/search.php">Search</a></li>
     <li><a href="/WhatsGoingOn/views/map.php">Map</a></li>
     <?php 
         if (isset($_SESSION['user_name'])) 
         {
-            echo "<li><a href='/WhatsGoingOn/createEventHandler.php'>Create Event</a></li>";
-            echo "<li><a href='/WhatsGoingOn/views/userProfile.php'>User Profile</a></li>";
+            echo "<li><a href='/WhatsGoingOn/createEventHandler.php'>Create</a></li>";
+            echo "<li><a href='/WhatsGoingOn/views/userProfile.php'>Profile</a></li>";
         }   
     ?>
 </ul>
