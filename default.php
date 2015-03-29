d98657d9d264251fc5a7c6f1bc1ea8bfa48867c1
Chris Wizner <wizner8398@fvtc.edu>
2015-03-29 16:56:28 -0500

    changed the tile

--- default.php
+++ default.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>Whats Going On?/Home</title>
+		<title>Whats Going On? - Home</title>
 		<meta name="description" content="">
 		<meta name="author" content="wetzel">
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
--- views/create_event.php
+++ views/create_event.php
@@ -19,7 +19,7 @@ if (isset($createEvent)) {
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>Whats Going On?/Create Event</title>
+		<title>Whats Going On? - Create Event</title>
 		<meta name="description" content="">
 		<meta name="author" content="wetzel">
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
