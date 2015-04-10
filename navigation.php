1d5b7f08d98cd96c34286f02be0ed8d1b23bbddc
AndrewJGifford <gifford.andrew.j@gmail.com>
2015-04-10 13:35:58 -0500

    Updated navigation to go to user's profile

--- navigation.php
+++ navigation.php
@@ -3,10 +3,10 @@
     <li><a href="/WhatsGoingOn/views/search.php">Search</a></li>
     <li><a href="/WhatsGoingOn/views/map.php">Map</a></li>
     <?php 
-        if (isset($_SESSION['user_name'])) 
+    if (isset($_SESSION['user_name'])) 
         {
             echo "<li><a href='/WhatsGoingOn/createEventHandler.php'>Create</a></li>";
-            echo "<li><a href='/WhatsGoingOn/views/userProfile.php'>Profile</a></li>";
+            echo "<li><a href='/WhatsGoingOn/views/userProfile.php?user=" . $_SESSION['user_name'] . "'>Profile</a></li>";
         }   
     ?>
 </ul>
