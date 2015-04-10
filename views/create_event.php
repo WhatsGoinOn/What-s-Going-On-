cefb464c5c39912e21b15f3b694c3371bd1c5b3d
Chris Wizner <wizner8398@fvtc.edu>
2015-04-10 09:32:41 -0500

    started the create event page

--- views/create_event.php
+++ views/create_event.php
@@ -12,6 +12,17 @@ if (isset($createEvent)) {
         }
     }
 }
+
+if (!isset($_SESSION))
+{
+    session_start();
+}
+
+if (!isset($_SESSION['user_name']))
+{
+    header('Location: /WhatsGoingOn/default.php');
+    die();
+}
 ?>
 
 <!DOCTYPE html>
@@ -36,10 +47,10 @@ if (isset($createEvent)) {
 		</style>
 		
 		<script type="text/javascript">
-        function Validate()
-        {           
-                    
-        }
+            function Validate()
+            {           
+                
+            }
         </script>       		
 
 	</head>
@@ -59,105 +70,154 @@ if (isset($createEvent)) {
 	  	</nav>
 	  
 	  	<section id="createEventSection">
-	  		<form id="createEvent" name="createEvent" method="post" action="#" onsubmit="">		
+	  		<form id="createEvent" name="createEvent" method="post" action="/WhatsGoingOn/createEventHandler.php">		
 			
 				<fieldset>
 					<legend>Create Event:</legend>
 					<div>
-						<label for="eventName">Event Name:</label>  
-		  					<input type="text" id="eventName" required/><br>						
+						<label for="title">Event Name:</label>  
+		  					<input type="text" id="title" name="title" placeholder="Name" required/><br>
+		  				Is this event free?
+                        <input type="radio" name="is_free" value="0">Yes 
+                        <input type="radio" name="is_free" value="1">No<br>
+		  				<label for="address">Address:</label>  
+                            <input type="text" id="address" name="address" placeholder="Address" required/><br>  					
 						<label for="city">City:</label>  
-			  				<input type="text" id="city" required/><br>	
-			  			<label for="state" id="stateLabel">State:</label>
-						<select name="state" id="state" required>
-							<option value="">Select</option>
-							<option value="AL">Alabama - AL</option>
-							<option value="AK">Alaska - AK</option>
-							<option value="AS">American Samoa - AS</option>
-							<option value="AK">Arizona - AZ</option>
-							<option value="AR">Arkansas - AR</option>
-							<option value="CA">California - CA</option>
-							<option value="CO">Colorado - CO</option>
-							<option value="CT">Connecticut - CT</option>
-							<option value="DE">Delaware - DE</option>
-							<option value="DC">District of Columbia - DC</option>
-							<option value="FM">Federated States of Micronesia - FM</option>
-							<option value="FL">Florida - FL</option>
-							<option value="GA">Georgia - GA</option>
-							<option value="GU">Guam - GU</option>
-							<option value="HI">Hawaii - HI</option>
-							<option value="ID">Idaho - ID</option>
-							<option value="IL">Illinois - IL</option>
-							<option value="IN">Indiana - IN</option>
-							<option value="IA">Iowa - IA</option>
-							<option value="KS">Kansas - KS</option>
-							<option value="KY">Kentucky - KY</option>
-							<option value="LA">Louisiana - LA</option>
-							<option value="ME">Maine - ME</option>
-							<option value="MH">Marshall Islands - MH</option>
-							<option value="MD">Maryland - MD</option>
-							<option value="MA">Massachusetts - MA</option>
-							<option value="MI">Michigan - MI</option>
-							<option value="MN">Minnesota - MN</option>
-							<option value="MS">Mississippi - MS</option>
-							<option value="MO">Missouri - MO</option>
-							<option value="MT">Montana - MT</option>
-							<option value="NE">Nebraska - NE</option>
-							<option value="NV">Nevada - NV</option>
-							<option value="NH">New Hampshire - NH</option>
-							<option value="NJ">New Jersey - NJ</option>
-							<option value="NM">New Mexico - NM</option>
-							<option value="NY">New York - NY</option>
-							<option value="NC">North Carolina - NC</option>
-							<option value="ND">North Dakota - ND</option>
-							<option value="MP">Northern Mariana Islands - MP</option>
-							<option value="OH">Ohio - OH</option>
-							<option value="OK">Oklahoma - OK</option>
-							<option value="OR">Oregon - OR</option>
-							<option value="PW">Palau - PW</option>
-							<option value="PA">Pennsylvania - PA</option>
-							<option value="PR">Puerto Rico - PR</option>
-							<option value="RI">Rhode Island - RI</option>
-							<option value="SC">South Carolina - SC</option>
-							<option value="SD">South Dakota - SD</option>
-							<option value="TN">Tennessee - TN</option>
-							<option value="TK">Texas - TX</option>
-							<option value="UT">Utah - UT</option>
-							<option value="VT">Vermont - VT</option>
-							<option value="VI">Virgin Island - VI</option>
-							<option value="VA">Virginia - VA</option>
-							<option value="WA">Washington - WA</option>
-							<option value="WV">West Virginia - WV</option>
-							<option value="WI">Wisconsin - WI</option>
-							<option value="WY">Wyoming - WY</option>
-						</select><br>	
+			  				<input type="text" id="city" name="city" placeholder="City" required/><br>			  		    
+			  			<label for="state">State:</label>
+    						<select name="state" id="state" required>
+    							<option value="">Select State</option>
+    							<option value="AL">Alabama</option>
+    							<option value="AK">Alaska</option>
+    							<option value="AS">American Samoa</option>
+    							<option value="AZ">Arizona</option>
+    							<option value="AR">Arkansas</option>
+    							<option value="CA">California</option>
+    							<option value="CO">Colorado</option>
+    							<option value="CT">Connecticut</option>
+    							<option value="DE">Delaware</option>
+    							<option value="DC">District of Columbia</option>
+    							<option value="FM">Federated States of Micronesia</option>
+    							<option value="FL">Florida</option>
+    							<option value="GA">Georgia</option>
+    							<option value="GU">Guam</option>
+    							<option value="HI">Hawaii</option>
+    							<option value="ID">Idaho</option>
+    							<option value="IL">Illinois</option>
+    							<option value="IN">Indiana</option>
+    							<option value="IA">Iowa</option>
+    							<option value="KS">Kansas</option>
+    							<option value="KY">Kentucky</option>
+    							<option value="LA">Louisiana</option>
+    							<option value="ME">Maine</option>
+    							<option value="MH">Marshall Islands</option>
+    							<option value="MD">Maryland</option>
+    							<option value="MA">Massachusetts</option>
+    							<option value="MI">Michigan</option>
+    							<option value="MN">Minnesota</option>
+    							<option value="MS">Mississippi</option>
+    							<option value="MO">Missouri</option>
+    							<option value="MT">Montana</option>
+    							<option value="NE">Nebraska</option>
+    							<option value="NV">Nevada</option>
+    							<option value="NH">New Hampshire</option>
+    							<option value="NJ">New Jersey</option>
+    							<option value="NM">New Mexico</option>
+    							<option value="NY">New York</option>
+    							<option value="NC">North Carolina</option>
+    							<option value="ND">North Dakota</option>
+    							<option value="MP">Northern Mariana Islands</option>
+    							<option value="OH">Ohio</option>
+    							<option value="OK">Oklahoma</option>
+    							<option value="OR">Oregon</option>
+    							<option value="PW">Palau</option>
+    							<option value="PA">Pennsylvania</option>
+    							<option value="PR">Puerto Rico</option>
+    							<option value="RI">Rhode Island</option>
+    							<option value="SC">South Carolina</option>
+    							<option value="SD">South Dakota</option>
+    							<option value="TN">Tennessee</option>
+    							<option value="TK">Texas</option>
+    							<option value="UT">Utah</option>
+    							<option value="VT">Vermont</option>
+    							<option value="VI">Virgin Island</option>
+    							<option value="VA">Virginia</option>
+    							<option value="WA">Washington</option>
+    							<option value="WV">West Virginia</option>
+    							<option value="WI">Wisconsin</option>
+    							<option value="WY">Wyoming</option>
+    						</select><br>	
 						<label for="zipCode">Zip Code:</label>  
-				  			<input type="text" id="zipCode"/> required<br>
-				  		<!--Insert the Calendar here -->
-	  					<h1>CALENDAR HERE</h1>	
-	  					<textarea rows="8" cols="50" placeholder="Type Description Here!"></textarea>/><br>
+				  			<input type="text" id="zipCode" name="zipCode" placeholder="five digits (ex - 12345)" required /><br>
+				  		<label for="startDate">Start Date:</label>  
+                            <input type="text" id="startDate" name="startDate" placeholder="Start Date" required /><br>
+                        <label for="startTime">Start Time:</label>
+                            <select name="startTime" id="startTime" required>
+                                <option value="">Select Start Time</option>
+                                <?php  
+                                    $min = "00";
+                                    $hour = 12;
+                                    
+                                    for ($i = 0; $i < 48; $i++){                                                                                
+                                        $value = $hour . ":" . $min;                                        
+                                        echo "<option value=" . $value . ">" . $value . "</option>";                                            
+                                        $min += 15;                                        
+                                        if ($min == 60){
+                                            $hour += 1;
+                                            $min = "00";
+                                        }
+                                        if ($hour == 13){
+                                            $hour = 1;
+                                        }
+                                    }
+                                ?>
+                            </select>
+                            <select name="start_am_pm" id="start_am_pm" required>
+                                <option value="">AM/PM</option>
+                                <option value="AM">AM</option>
+                                <option value="PM">PM</option>
+                            </select><br>
+                        <label for="endDate">End Date:</label>  
+                            <input type="text" id="endDate" name="endDate" placeholder="End Date" required /><br>
+                        <label for="endTime" id="endTimeLabel">End Time:</label>
+                            <select name="endTime" id="endTime" required>
+                                <option value="">Select End Time</option>
+                                <?php 
+                                $min = "00";
+                                $hour = 12;
+                                
+                                for ($i = 0; $i < 48; $i++){                                                                           
+                                    $value = $hour . ":" . $min;                                    
+                                    echo "<option value=" . $value . ">" . $value . "</option>";                                        
+                                    $min += 15;                                    
+                                    if ($min == 60){
+                                        $hour += 1;
+                                        $min = "00";
+                                    }
+                                    if ($hour == 13){
+                                        $hour = 1;
+                                    }
+                                }?>
+				  			</select>
+                            <select name="end_am_pm" id="end_am_pm" required>
+                                <option value="">AM/PM</option>
+                                <option value="AM">AM</option>
+                                <option value="PM">PM</option>
+                            </select><br>
+	  					<textarea rows="8" cols="50" id="description" name="description" placeholder="Description Here!"></textarea><br>
 						<img src="eventImage.jpg" alt="IMAGE HERE!" width="200" height="200"><br>
-						<label for="browseImage">Select Image</label>	
-							<input type="button" id="browseImage" value="Browse" onclick=""/><br>
-						<p>Select appropriate tags:</p>
-						<input type="checkbox" id="music"/>
-							<label for="music">Music</label><br>
-						<input type="checkbox" id="sports"/>
-				  			<label for="sports">Sports</label><br>	
-				  		<input type="checkbox" id="annual"/>	  			
-				  			<label for="annual">Annual</label><br>
-				  		<input type="checkbox" id="party"/>
-				  			<label for="party">Party</label><br>
-				  		<input type="checkbox" id="kids"/>
-				  			<label for="kids">Kids</label><br>
-				  		<input type="checkbox" id="school"/>
-				  			<label for="school">School</label><br>
-				  		<input type="checkbox" id="public"/>
-				  			<label for="public">Public</label><br>
-				  		<input type="checkbox" id="community"/>
-				  			<label for="community">Community</label><br>
-				  		<input type="checkbox" id="private"/>
-				  			<label for="private">Private</label><br>	
+						<input type="button" id="browseImage" value="Select Image" onclick=""/><br>
+						<p>Select appropriate tags (min 1):</p><br>
+						<input type="checkbox" id="music" name="chk_tags[]" value="Music" />Music<br>							
+						<input type="checkbox" id="sports" name="chk_tags[]" value="Sports" />Sports<br>				  			
+				  		<input type="checkbox" id="annual" name="chk_tags[]" value="Annual" />Annual<br>				  			
+				  		<input type="checkbox" id="party" name="chk_tags[]" value="Party" />Party<br>				  			
+				  		<input type="checkbox" id="kids" name="chk_tags[]" value="Kids" />Kids<br>				  			
+				  		<input type="checkbox" id="school" name="chk_tags[]" value="School" />School<br>				  			
+				  		<input type="checkbox" id="public" name="chk_tags[]" value="Public" />Public<br>				  			
+				  		<input type="checkbox" id="community" name="chk_tags[]" value="Community" />Community<br>				  			
+				  		<input type="checkbox" id="private" name="chk_tags[]" value="Private" />Private<br>
+				  				
 				  		<input type="submit" value="Create Event" onclick="return Validate()" name="createEvent"/>			  			
 					</div>
 				</fieldset>	
