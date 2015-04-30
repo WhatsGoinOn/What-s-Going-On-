9eb821ca200aab5b748f1452524080ec9ee99710
unknown <Andrew@.(none)>
2015-04-30 16:23:42 -0500

    testing date and time

--- event.php
+++ event.php
@@ -70,7 +70,7 @@
 		  			<div class="eventImage">
 		  				<img class="imgSub" src="image.php?id=<?php echo($event->ImageID); ?>" alt="Event image">
 		  			</div>
-		  			<?php $startDate = date("d/m/Y", strtotime($event->StartDateTime));?>
+		  			<?php $startDate = date("d/m/Y H:i:s", strtotime($event->StartDateTime));?>
 		  			<p><?php echo($startDate)?></p>
 		  			<p><?php echo($event->Address);?></p>
 		  			<p><?php echo($event->City);?>&nbsp;<?php echo($event->State);?>&nbsp;<?php echo($event->ZIP);?></p>
